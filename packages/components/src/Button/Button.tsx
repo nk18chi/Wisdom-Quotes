4806070d631522d75034a4c4c305896407325916
Naoki Mita <naoki.mita18@gmail.com>
2024-04-13 21:51:49 -0700

    style(ButtonComponent): remove red color style in hover

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import { Button as MuiButton, ButtonProps as MuiButtonProps } from "@mui/material";
+import { css } from '@emotion/react';
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+} from '@mui/material';
 
 export interface ButtonProps extends MuiButtonProps {
   label: string;
@@ -11,7 +14,6 @@ const ButtonStyles = css`
   color: white;
   :hover {
     opacity: 0.8;
-    color: red;
   }
 `;
 

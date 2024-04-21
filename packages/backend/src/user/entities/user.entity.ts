a69a0431a3677c3095f204206e13c8f29126d21f
Naoki Mita <naoki.mita18@gmail.com>
2024-04-20 22:30:03 -0700

    feat(Backend): fetch author field with user

--- packages/backend/src/user/entities/user.entity.ts
+++ packages/backend/src/user/entities/user.entity.ts
@@ -1,9 +1,13 @@
 import { ObjectType, Field } from '@nestjs/graphql';
+import { Author } from '../../author/entities/author.entity';
 
 @ObjectType()
 export class User {
   @Field(() => String, { description: 'The email of User' })
   email: number;
+
+  @Field(() => Author, { description: 'The author of User', nullable: true })
+  author?: Author;
 
   @Field(() => Date, { description: 'The creation date of the User' })
   createdAt: string;
--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -61,6 +61,9 @@ export class UserService {
       where: {
         id,
       },
+      include: {
+        author: true,
+      },
     });
   }
 }

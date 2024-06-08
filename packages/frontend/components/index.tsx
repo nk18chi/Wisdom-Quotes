2d98f5d24e3dcbe8b38c770651e4f71ab432f377
Naoki Mita <naoki.mita18@gmail.com>
2024-06-07 21:53:28 -0700

    test(AppBar): add unit test for AppBar component

--- packages/frontend/components/index.tsx
+++ packages/frontend/components/index.tsx
@@ -1,3 +1,3 @@
-export { default as AppBar } from './AppBar';
+export { default as AppBar } from './AppBar/AppBar';
 export { default as Hero } from './Hero/Hero';
 export { default as ArticleList } from './List/ArticleList';
